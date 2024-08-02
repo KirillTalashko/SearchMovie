f7165eea9b26d7ce54f570ff9f00c87a2bae73e0
Kirill Talashko <KirillTalashk@yandex.ru>
2024-08-02 12:35:41 +0300

    Create Custom View

---
+++ app/src/main/java/com/example/searchmovie/PlayTrailerView.kt
@@ -0,0 +1,15 @@
+package com.example.searchmovie
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.cardview.widget.CardView
+
+class PlayTrailerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : CardView(context,attrs,defStyleAttr) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.screen_play_trailer, this, false)
+    }
+}
